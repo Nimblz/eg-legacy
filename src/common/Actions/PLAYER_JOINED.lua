42c06eaecb0322d6cf3a4785b25485c8549aaf94
Nim <areuschle1@gmail.com>
2019-02-17 21:57:25 -0600

    added filler save data

--- src/common/Actions/PLAYER_JOINED.lua
+++ src/common/Actions/PLAYER_JOINED.lua
@@ -4,7 +4,17 @@ local PLAYER_ADD = require(script.Parent:WaitForChild("PLAYER_ADD"))
 
 return function(player)
     return function(store)
-        local saveData = {} -- TODO: Get save data
+        local saveData = {
+            portals = {
+                red = true,
+                orange = false,
+                yellow = true,
+                green = false,
+                cyan = false,
+                blue = true,
+                purple = true
+            }
+        } -- TODO: Get real save data
 
         store:dispatch(PLAYER_ADD(player,saveData))
     end
