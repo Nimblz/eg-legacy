161aa476c1f8911ac320fa2e59dc6c9cc31f2a1a
Nim <areuschle1@gmail.com>
2019-02-17 21:43:49 -0600

    tied actions into one module

---
+++ src/common/Actions/init.lua
@@ -0,0 +1,8 @@
+return {
+    COIN_ADD = require(script.COIN_ADD),
+    COIN_SUBTRACT = require(script.COIN_SUBTRACT),
+    PLAYER_ADD = require(script.PLAYER_ADD),
+    PLAYER_REMOVE = require(script.PLAYER_REMOVE),
+    PLAYER_JOINED = require(script.PLAYER_JOINED),
+    PLAYER_LEAVING = require(script.PLAYER_LEAVING),
+}
