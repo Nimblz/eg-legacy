11d3938bb9b93f8a7db84d0093507288e7d69668
Nim <areuschle1@gmail.com>
2019-02-17 21:50:41 -0600

    implemented rodux store

--- src/server/Server.server.lua
+++ src/server/Server.server.lua
@@ -4,19 +4,28 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local common = ReplicatedStorage:WaitForChild("common")
 local commonUtil = common:WaitForChild("util")
-
+local lib = ReplicatedStorage:WaitForChild("lib")
 local moduleBin = ServerScriptService:WaitForChild("module")
 
+local Rodux = require(lib:WaitForChild("Rodux"))
 local FuncUtil = require(commonUtil:WaitForChild("FuncUtil"))
+
+local serverReducer = require(ServerScriptService:WaitForChild("serverReducer"))
 
 local Server = {}
 
 Server.modules = {
+	PlayerHandler = require(moduleBin:WaitForChild("PlayerHandler")),
 	TipJarProducts = require(moduleBin:WaitForChild("TipJarProducts")),
 	TimeBadges = require(moduleBin:WaitForChild("TimeBadges")),
 	CharacterHandler = require(moduleBin:WaitForChild("CharacterHandler")),
 	Coins = require(moduleBin:WaitForChild("Coins")),
 }
+
+Server.store = Rodux.Store.new(serverReducer, nil, {
+	Rodux.thunkMiddleware,
+	Rodux.loggerMiddleware,
+})
 
 function Server:getModule(name)
 	assert(self.modules[name],"No such module: "..name)
