69c16bde293935abb7337e824a01bdab334f44f2
Nim <areuschle1@gmail.com>
2019-02-18 22:25:13 -0600

    added dummy save data

--- src/common/Actions/PLAYER_JOINED.lua
+++ src/common/Actions/PLAYER_JOINED.lua
@@ -13,6 +13,24 @@ return function(player)
                 cyan = false,
                 blue = true,
                 purple = true
+            },
+
+            stats = {
+                playTime = 420, -- in mins
+                playedYesterday = true,
+                consecutiveDays = 5,
+                metersWalked = 9001,
+                coins = 1337,
+            },
+
+            achievements = {
+                joinGame = true,
+                coins1000 = true,
+                touchLava = true,
+                discoverForestTemple = true,
+                discoverScoob = true,
+                discoverSkyIsland = true,
+                activateAllPortals = false,
             }
         } -- TODO: Get real save data
 
