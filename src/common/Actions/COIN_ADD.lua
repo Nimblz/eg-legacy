c0db21e6b6d8810bd071146055b04727cb5da106
Nim <areuschle1@gmail.com>
2019-02-21 21:42:10 -0600

    assertions unneccesary

--- src/common/Actions/COIN_ADD.lua
+++ src/common/Actions/COIN_ADD.lua
@@ -1,7 +1,4 @@
 return function(player, coins)
-    assert(typeof(player)=="instance" and player:IsA("Player"), "expected type Player in arg 1") -- i should really be using t
-    assert(type(coins)=="number", "expected type Number in arg 2")
-
     return {
         type = script.Name,
         player = player,
