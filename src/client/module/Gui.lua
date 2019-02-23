ab0e27cf4aba3acc106a7a1b6f14a538be9fda28
Nim <areuschle1@gmail.com>
2019-02-22 21:57:41 -0600

    gui base

--- src/client/module/Gui.lua
+++ src/client/module/Gui.lua
@@ -1,3 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local lib = ReplicatedStorage:WaitForChild("lib")
+
+local Roact = require(lib:WaitForChild("Roact"))
+local RoactRodux = require(lib:WaitForChild("Roact"))
+
 local Gui = {}
 
 function Gui:init()
@@ -5,7 +13,7 @@ function Gui:init()
 end
 
 function Gui:start(client)
-
+    
 end
 
 return Gui
