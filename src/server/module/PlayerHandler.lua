f78ae40e167ace00e682ac802fd9a7429662c4c0
Nim <areuschle1@gmail.com>
2019-02-27 23:57:29 -0600

    moved thunks to dedicated folder

--- src/server/module/PlayerHandler.lua
+++ src/server/module/PlayerHandler.lua
@@ -1,9 +1,8 @@
 local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local common = ReplicatedStorage:WaitForChild("common")
+local server = script.Parent.Parent
 
-local Actions = require(common:WaitForChild("Actions"))
+local Thunks = require(server:WaitForChild("thunks"))
 
 local PlayerHandler = {}
 
@@ -15,11 +14,11 @@ function PlayerHandler:start(server)
     local store = server.store
     local api = server.api
     Players.PlayerAdded:Connect(function(player)
-        store:dispatch(Actions.PLAYER_JOINED(player,api))
+        store:dispatch(Thunks.PLAYER_JOINED(player,api))
     end)
 
     Players.PlayerRemoving:Connect(function(player)
-        store:dispatch(Actions.PLAYER_LEAVING(player))
+        store:dispatch(Thunks.PLAYER_LEAVING(player))
     end)
 end
 
---
+++ src/server/thunks/init.lua
@@ -0,0 +1,4 @@
+return {
+    PLAYER_JOINED = require(script:WaitForChild("PLAYER_JOINED")),
+    PLAYER_LEAVING = require(script:WaitForChild("PLAYER_LEAVING")),
+}
