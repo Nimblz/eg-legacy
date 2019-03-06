d6eafe38dd77be9ea2d5de0e326965a6b4f39ab3
Nim <areuschle1@gmail.com>
2019-03-05 21:38:51 -0600

    offline testing support

--- src/server/thunks/PLAYER_JOINED.lua
+++ src/server/thunks/PLAYER_JOINED.lua
@@ -4,17 +4,18 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
 
-local PlayerDataStore = require(lib:WaitForChild("PlayerDataStore"))
-
 local Actions = require(common:WaitForChild("Actions"))
 
 local PLAYER_ADD = Actions.PLAYER_ADD
 
 return function(player,api)
     return function(store)
-        local saveData = PlayerDataStore:GetSaveData(player)
-
-        local playerSaveTable = saveData:Get("playerSaveTable") or {}
+        local playerSaveTable = {}
+        if game.PlaceId ~= 0 then
+            local PlayerDataStore = require(lib:WaitForChild("PlayerDataStore"))
+            local saveData = PlayerDataStore:GetSaveData(player)
+            playerSaveTable = saveData:Get("playerSaveTable") or {}
+        end
 
         store:dispatch(PLAYER_ADD(player,playerSaveTable))
 
--- src/server/thunks/PLAYER_LEAVING.lua
+++ src/server/thunks/PLAYER_LEAVING.lua
@@ -4,8 +4,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
 
-local PlayerDataStore = require(lib:WaitForChild("PlayerDataStore"))
-
 local Actions = require(common:WaitForChild("Actions"))
 
 local PLAYER_REMOVE = Actions.PLAYER_REMOVE
@@ -13,12 +11,15 @@ local PLAYER_REMOVE = Actions.PLAYER_REMOVE
 return function(player)
     return function(store)
         local playerSaveTable = store:getState().players[player]
+        if game.PlaceId ~= 0 then
+            if playerSaveTable then
+                local PlayerDataStore = require(lib:WaitForChild("PlayerDataStore"))
+                local saveData = PlayerDataStore:GetSaveData(player)
 
-        if playerSaveTable then
-            local saveData = PlayerDataStore:GetSaveData(player)
-
-            saveData:Set("playerSaveTable", playerSaveTable)
+                saveData:Set("playerSaveTable", playerSaveTable)
+            end
         end
+
         store:dispatch(PLAYER_REMOVE(player))
     end
 end
