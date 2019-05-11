041d5e280b62f001b4e5bd352ac92a47a478b9e5
Nim <areuschle1@gmail.com>
2019-05-10 22:56:39 -0500

    day/night cycle

---
+++ src/server/module/DayNight.lua
@@ -0,0 +1,21 @@
+local Lighting = game:GetService("Lighting")
+local RunService = game:GetService("RunService")
+
+local MinsInDay = 15
+
+local DayNight = {}
+
+function DayNight:init()
+
+end
+
+function DayNight:start(client)
+    RunService.Stepped:connect(function()
+        local scaledToDay = tick() * 60 * 24 -- 1 sec 1 day
+        local scaledToMins = scaledToDay / (60 * MinsInDay) -- time for full cycle
+        local minsAfterMidnight = scaledToMins % 1440
+        Lighting:SetMinutesAfterMidnight(minsAfterMidnight)
+    end)
+end
+
+return DayNight
