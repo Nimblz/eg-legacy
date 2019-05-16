b77c7e0643f5df408c6c33bef307342e97fc66ad
Nim <areuschle1@gmail.com>
2019-05-16 16:21:50 -0500

    beginning to implement equipment

---
+++ src/client/module/LocalEquipmentRenderer.lua
@@ -0,0 +1,2 @@
+-- renders yours and other egs equipment and handles their local usage effects
+
--- src/common/AssetCatagories.lua
+++ src/common/AssetCatagories.lua
@@ -3,37 +3,37 @@ local by = require(script.Parent.util:WaitForChild("by"))
 local catagories = {
     {
         id = "hat",
-        image = "rbxassetid://3046056467", -- image used to represent this catagory
+        image = "rbxassetid://3185662736", -- image used to represent this catagory
         name = "Hat",
         maxEquipped = 3,
     },
     {
         id = "face",
-        image = "rbxassetid://3046056467",
+        image = "rbxassetid://3185662253",
         name = "Face",
         maxEquipped = 1,
     },
     {
         id = "material",
-        image = "rbxassetid://3046056467",
+        image = "rbxassetid://3185662381",
         name = "Material",
         maxEquipped = 1,
     },
     {
         id = "effect",
-        image = "rbxassetid://3046056467",
+        image = "rbxassetid://3185662510",
         name = "Effect",
         maxEquipped = 2,
     },
     {
         id = "tool",
-        image = "rbxassetid://3046056467",
+        image = "rbxassetid://3185662627",
         name = "Tool",
         maxEquipped = 3,
     },
     {
         id = "ability",
-        image = "rbxassetid://3046056467",
+        image = "rbxassetid://3185662029",
         name = "Ability",
         maxEquipped = 99,
     },
