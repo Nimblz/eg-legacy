3ed51673dd6c484af27baf075fe873574d066298
Nim <areuschle1@gmail.com>
2019-05-17 13:23:34 -0500

    new devproducts compilation

--- src/common/DevProducts/init.lua
+++ src/common/DevProducts/init.lua
@@ -1,4 +1,20 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local MarketplaceService = game:GetService("MarketplaceService")
 local common = ReplicatedStorage:WaitForChild("common")
 
-return require(common.util:WaitForChild("compileSubmodules"))(script)
+local by = require(common.util:WaitForChild("by"))
+local products = require(common.util:WaitForChild("compileSubmodulesToArray"))(script, true)
+
+local function isLessExpensive(asset1,asset2)
+    local asset1Info = MarketplaceService:GetProductInfo(asset1.productId, Enum.InfoType.Product)
+    local asset2Info = MarketplaceService:GetProductInfo(asset2.productId, Enum.InfoType.Product)
+
+    return asset1Info.PriceInRobux < asset2Info.PriceInRobux
+end
+
+table.sort(products,isLessExpensive)
+
+return {
+    all = products,
+    byId = by("id", products),
+}
