67fcfb8e5bbcacff340ee24cecf821a4f8b96caf
Nim <areuschle1@gmail.com>
2019-05-17 13:24:18 -0500

    donations moved to their own folder

--- src/common/DevProducts/donations/BigSupport.lua
+++ src/common/DevProducts/donations/BigSupport.lua
@@ -2,7 +2,7 @@ return {
     name = script.Name,
     productId = 467033474,
 
-    onProductPurchase = (function(player, server, product)
+    onProductPurchase = (function(player, server)
         -- Do nothing, this is a donation
 
         return true -- Successful
