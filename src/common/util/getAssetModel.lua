b01349728f39c2cc21f4ca09b1068f1a424e11d3
Nim <areuschle1@gmail.com>
2019-05-19 10:20:40 -0500

    generate asset previews for faces

--- src/common/util/getAssetModel.lua
+++ src/common/util/getAssetModel.lua
@@ -9,11 +9,20 @@ local cataFuncs = {
     hat = function(id, asset)
         return models:WaitForChild("hats"):FindFirstChild(id)
     end,
+    face = function(id, asset)
+        local egbase = models:WaitForChild("materials").materialbase:Clone()
+
+        egbase.Decal.Texture = asset.metadata.image or "rbxassetid://102312301"
+
+        return egbase
+    end,
     material = function(id, asset)
         local materialSample = models:WaitForChild("materials").materialbase:Clone()
 
         materialSample.Color = asset.metadata.color or BrickColor.new("White").Color
         materialSample.Material = asset.metadata.material or Enum.Material.Plastic
+        materialSample.Reflectance = asset.metadata.reflectance or 0
+        materialSample.Transparency = asset.metadata.transparency or 0
 
         return materialSample
     end
