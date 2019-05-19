59798c75ec9be1bdd6e9b5566b9f6f2c034f843c
Nim <areuschle1@gmail.com>
2019-05-19 10:47:20 -0500

    starter save no longer needs coins

--- src/common/Thunks/PLAYER_JOINED.lua
+++ src/common/Thunks/PLAYER_JOINED.lua
@@ -38,7 +38,7 @@ return function(player,api)
                 -- will not retroactively get them.
             },
             stats = {
-                coins = 2500,
+                coins = 0,
             }
         }
         if game.PlaceId ~= 0 then
