d5ed10473c73dfb3138169d4c7cc37fd8ee94aab
Nim <areuschle1@gmail.com>
2019-05-19 10:18:06 -0500

    command to give everyone money

--- misc/cheats.lua
+++ misc/cheats.lua
@@ -33,3 +33,15 @@ while #coinBin:GetChildren() > 0 and _G.coinGrabbing do
     RunService.RenderStepped:Wait() -- only wait 1 frame
 end
 _G.coinGrabbing = false
+
+-- You gotta have access to server console to use below --
+
+-- give everyone in server coins
+
+local Players = game:GetService("Players")
+local amount = 2500
+local server = _G.server
+
+for _, player in pairs(Players:GetPlayers()) do
+    server.store:dispatch({type = "COIN_ADD", coins = amount, player = player, replicateTo = player})
+end
