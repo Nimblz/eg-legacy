d9b0d64f6d27540ac85863c7f09f51f4e43edd99
Nim <areuschle1@gmail.com>
2019-05-19 07:11:42 -0500

    fixed asset give thunk

--- src/common/Thunks/ASSET_TRYGIVE.lua
+++ src/common/Thunks/ASSET_TRYGIVE.lua
@@ -1,10 +1,10 @@
 local Assets = require(script.Parent.Parent:WaitForChild("Assets"))
 local Actions = require(script.Parent.Parent:WaitForChild("Actions"))
 
-return function(assetId)
+return function(player, assetId)
     return function(store)
         if Assets.byId[assetId] then
-            store:dispatch(Actions.ASSET_GIVE(assetId))
+            store:dispatch(Actions.ASSET_GIVE(player, assetId))
         else
             warn(("Assset %s does not exist!"):format(assetId))
         end
