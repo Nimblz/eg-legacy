a46dcd0597500cb5f3e2b8e55990437350194272
Nim <areuschle1@gmail.com>
2019-05-19 07:12:50 -0500

    0.3.1 release

---
+++ src/common/VersionInfo/Versions/0.3.1.lua
@@ -0,0 +1,10 @@
+return {
+    id = 5,
+    version = "0.3.1",
+    date = "05/18/2019",
+    changelog = {
+        "Fixed data loss bug",
+        "Fixed billboard names for artists",
+        "Free starter hats and materials on join",
+    }
+}
