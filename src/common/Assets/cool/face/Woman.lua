0d7453bbd2f4b2974d0560504c7ee89104d35cd0
Nim <areuschle1@gmail.com>
2019-05-19 10:46:56 -0500

    woman face

---
+++ src/common/Assets/cool/face/Woman.lua
@@ -0,0 +1,12 @@
+return {
+    id = "face_woman",
+    type = "face",
+    name = "Womanly Face",
+    description = ":)",
+    rarity = 1,
+    hidden = false,
+
+    metadata = {
+        image = "rbxassetid://83022608"
+    }
+}
---
+++ src/common/ShopProducts/faces/Woman.lua
@@ -0,0 +1,5 @@
+return {
+    id = "face_woman",
+    price = 500,
+    onSale = true,
+}
