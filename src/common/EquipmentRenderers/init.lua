8c6bcac30bc1b2f9900add1c8aefa9fc8662f1bb
Nim <areuschle1@gmail.com>
2019-05-19 10:19:57 -0500

    implemented face renderer

--- src/common/EquipmentRenderers/init.lua
+++ src/common/EquipmentRenderers/init.lua
@@ -1,5 +1,6 @@
 return {
     HatRenderer = require(script:WaitForChild("HatRenderer")),
+    FaceRenderer = require(script:WaitForChild("FaceRenderer")),
     MaterialRenderer = require(script:WaitForChild("MaterialRenderer")),
     RainbowMaterialRenderer = require(script:WaitForChild("RainbowMaterialRenderer")),
 }
