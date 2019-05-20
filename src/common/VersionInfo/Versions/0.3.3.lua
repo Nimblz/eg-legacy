c82f60148d0bde4fb40516102f17617b56d625ce
Nim <areuschle1@gmail.com>
2019-05-19 21:01:50 -0500

    0.3.4 release

---
+++ src/common/VersionInfo/Versions/0.3.3.lua
@@ -0,0 +1,8 @@
+return {
+    id = 6,
+    version = "0.3.3",
+    date = "05/19/2019",
+    changelog = {
+        "Coin achievements changed to be at 5000 and 100000 coins.",
+    }
+}
---
+++ src/common/VersionInfo/Versions/0.3.4.lua
@@ -0,0 +1,8 @@
+return {
+    id = 6,
+    version = "0.3.4",
+    date = "05/19/2019",
+    changelog = {
+        "Fixed invisible hats on join bug",
+    }
+}
