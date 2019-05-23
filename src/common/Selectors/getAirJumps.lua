d996da8cfb7921cefe44b435691c2f64e900dd03
Nim <areuschle1@gmail.com>
2019-05-23 14:10:09 -0500

    movement ability selectors

---
+++ src/common/Selectors/getAirJumps.lua
@@ -0,0 +1,8 @@
+local getStats = require(script.Parent.getStats)
+
+return function(state, player)
+    local pstats = getStats(state,player)
+    if pstats then
+        return getStats(state,player).airJumps
+    end
+end
