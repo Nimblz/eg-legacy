31f0a0719a56f5d566b6618da92f9399a3b3a1d5
Nim <areuschle1@gmail.com>
2019-05-24 16:06:20 -0500

    0.3.7 version

---
+++ src/common/VersionInfo/Versions/0.3.7.lua
@@ -0,0 +1,10 @@
+return {
+    id = 11,
+    version = "0.3.7",
+    date = "05/23/2019",
+    changelog = {
+        "Pets!",
+        "Movement ability backend",
+        "Price adjustments for devproducts and shop items"
+    }
+}
