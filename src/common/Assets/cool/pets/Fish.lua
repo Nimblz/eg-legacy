07ef7f693f028eee29544d149a27a0e0fa9a8cc0
Nim <areuschle1@gmail.com>
2019-05-27 21:35:40 -0500

    fish pet

---
+++ src/common/Assets/cool/pets/Fish.lua
@@ -0,0 +1,11 @@
+return {
+    id = "pet_fish",
+    name = "Big Fish",
+    description = "large and in charge",
+    type = "pet",
+    rarity = 3,
+    hidden = false,
+
+    metadata = {
+    }
+}
