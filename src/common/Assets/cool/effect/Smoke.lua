5da9f0c90abaeb8c252436d248a8c1f2b0cbce3a
Austin Reuschle <areuschle@roblox.local>
2019-10-06 20:21:49 -0700

    Add smoke effect

---
+++ src/common/Assets/cool/effect/Smoke.lua
@@ -0,0 +1,16 @@
+return {
+    id = "effect_smoke",
+    type = "effect",
+    name = "Smoke",
+    description = "n/a",
+    rarity = 4,
+    hidden = false,
+    thumbnailImage = "rbxassetid://4065545303",
+
+    metadata = {
+        effectType = "particle",
+        effectInstances = {
+            "effect_smoke",
+        }
+    }
+}
