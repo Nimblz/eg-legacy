6a5b8dd8d77140b291e4656f9f336af9bf7e2229
Austin Reuschle <areuschle@roblox.local>
2019-10-13 11:17:04 -0700

    Remove testing prints

--- src/common/Thunks/ASSET_TRYBUY.lua
+++ src/common/Thunks/ASSET_TRYBUY.lua
@@ -12,11 +12,8 @@ return function(player,assetId)
     return function(store)
         local product = ShopProducts.byId[assetId]
         if product then
-            print("product exists")
             if not Selectors.isOwned(store:getState(),player,assetId) then
-                print("not owned trying to buy")
                 if Selectors.getCoins(store:getState(),player) >= product.price then
-                    print("bought")
                     store:dispatch(Actions.COIN_SUBTRACT(player,product.price))
                     store:dispatch(Actions.ASSET_GIVE(player,assetId))
                 end
