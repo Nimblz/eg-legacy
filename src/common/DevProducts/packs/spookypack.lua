ecff3d4ea25be6c1965f2823040678412beb3552
Austin Reuschle <areuschle@roblox.local>
2019-10-13 11:17:27 -0700

    Add spooky item pack

--- src/common/DevProducts/packs/spookypack.lua
+++ src/common/DevProducts/packs/spookypack.lua
@@ -5,13 +5,13 @@ local common = ReplicatedStorage:WaitForChild("common")
 local Thunks = require(common:WaitForChild("Thunks"))
 
 return {
-    id = "itempack_rainbow",
-    name = "Rainbow Item Pack",
+    id = "itempack_spooky",
+    name = "Spooky Item Pack",
     productId = 545309170,
     onSale = true,
 
     order = 0,
-    flavorText = "SALE!",
+    flavorText = "EVENT!",
 
     onProductPurchase = (function(player, server)
         server:getModule("StoreContainer"):getStore():andThen(function(store)
