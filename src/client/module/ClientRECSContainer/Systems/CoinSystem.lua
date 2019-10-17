31d527e825df35d3425fe5d5de4e2d9cc0e3c3b8
Austin Reuschle <areuschle@roblox.local>
2019-10-17 15:27:43 -0700

    Simplify coin system

--- src/client/module/ClientRECSContainer/Systems/CoinSystem.lua
+++ src/client/module/ClientRECSContainer/Systems/CoinSystem.lua
@@ -15,16 +15,7 @@ local hitIsYou = require(util:WaitForChild("hitIsYou"))
 
 local CoinSystem = RECS.System:extend("CoinSystem")
 
-local ANIM_THRESHOLD = 180
-local MAX_COINS_PER_FRAME = 128
 local COIN_SOUND_ID = 1453122289
-local TILE_SIZE = 128
-
-local function pointsCloserThan(p1,p2,dist)
-    local delta = p1-p2
-
-    return ((delta.X*delta.X) + (delta.Y*delta.Y) + (delta.Z*delta.Z)) <= dist*dist
-end
 
 function CoinSystem:coinTouched(coin, instance, hit)
     if hitIsYou(hit) then
@@ -40,55 +31,13 @@ function CoinSystem:coinTouched(coin, instance, hit)
     end
 end
 
-function CoinSystem:getCoinTile(x,y)
-    if not self.coinTiles[x] then
-        return
-    end
-    if not self.coinTiles[x][y] then
-        return
-    end
-
-    return self.coinTiles[x][y]
-end
-
-function CoinSystem:addCoinToTile(x,y,instance,coin)
-    if not self.coinTiles[x] then
-        self.coinTiles[x] = {}
-    end
-    if not self.coinTiles[x][y] then
-        self.coinTiles[x][y] = {}
-    end
-
-    self.coinTiles[x][y][instance] = coin
-end
-
-function CoinSystem:removeCoinFromTile(x,y,instance)
-    if not self.coinTiles[x] then
-        return
-    end
-    if not self.coinTiles[x][y] then
-        return
-    end
-
-    self.coinTiles[x][y][instance] = nil
-end
-
 function CoinSystem:addCoin(instance,coin)
-    coin.originalCFrame = instance.CFrame
-    coin.timeOffset = math.random() * 2 * math.pi
-    local coinPos = instance.CFrame.p
-    local tileX = math.floor(coinPos.X/TILE_SIZE)
-    local tileY = math.floor(coinPos.Z/TILE_SIZE)
-
     instance.Touched:Connect(function(hit)
         self:coinTouched(coin, instance, hit)
-        self:removeCoinFromTile(tileX,tileY,instance)
     end)
-    self:addCoinToTile(tileX,tileY,instance,coin)
 end
 
 function CoinSystem:init()
-    self.coinTiles = {}
 
     self.maid.componentAdded =
         self.core:getComponentAddedSignal(Components.Coin):Connect(
@@ -98,52 +47,6 @@ function CoinSystem:init()
 
     for instance, coin in self.core:components(Components.Coin) do
         self:addCoin(instance,coin)
-    end
-end
-
-function CoinSystem:doCoinAnimationStep(instance,coin)
-    local time = tick() + coin.timeOffset
-    local sinVal = math.sin(time % (math.pi*2))
-    local rotation = (time/2) % (math.pi*2)
-
-    sinVal = sinVal * sinVal
-
-    instance.CFrame = instance.CFrame:lerp(
-        coin.originalCFrame *
-        CFrame.new(0,sinVal*1,0) *
-        CFrame.Angles(0,rotation,0),
-        0.1
-    )
-end
-
-function CoinSystem:step()
-    local coinsAnimated = 0
-    local camera = Workspace.CurrentCamera
-    local camPos = camera.CFrame.p
-
-    local camTileX = math.floor(camPos.X/TILE_SIZE)
-    local camTileY = math.floor(camPos.Z/TILE_SIZE)
-
-    for x = -1,1 do
-        for y = -1,1 do
-            local coinTileX = camTileX+x
-            local coinTileY = camTileY+y
-
-            local coinTile = self:getCoinTile(coinTileX,coinTileY)
-            if coinTile then
-                for instance, coin in pairs(coinTile) do
-                    local coinPos = coin.originalCFrame.p
-                    local shouldSpin = pointsCloserThan(camPos,coinPos, ANIM_THRESHOLD)
-                    if shouldSpin and coinsAnimated < MAX_COINS_PER_FRAME then
-                        coinsAnimated = coinsAnimated + 1
-                        self:doCoinAnimationStep(instance,coin)
-                    end
-                    if coinsAnimated >= MAX_COINS_PER_FRAME then
-                        return
-                    end
-                end
-            end
-        end
     end
 end
 
