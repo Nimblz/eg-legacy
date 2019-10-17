6bf6fda577c8a1c212de215e690963fb149a8ab4
Austin Reuschle <areuschle@roblox.local>
2019-10-17 15:33:11 -0700

    Update version

--- src/common/VersionInfo/Versions/0.5.0.lua
+++ src/common/VersionInfo/Versions/0.5.0.lua
@@ -1,11 +1,14 @@
 return {
     id = 19,
     version = "0.5.0",
-    date = "10/15/2019",
+    date = "10/17/2019",
     changelog = {
         "Spooky map theme!",
         "New halloween particles!",
         "New halloween hats!",
+        "New halloween pets!",
         "New candy currency",
+        "Changelog actually implemented!",
+        "Badges temporarily disabled",
     }
 }
