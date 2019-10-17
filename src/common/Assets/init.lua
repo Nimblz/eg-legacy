8d95b3fd7cc64716fe320f8d1933b6c8c0a72ac7
Austin Reuschle <areuschle@roblox.local>
2019-10-17 15:29:02 -0700

    Set shop catagories

--- src/common/Assets/init.lua
+++ src/common/Assets/init.lua
@@ -5,6 +5,10 @@ local by = require(common.util:WaitForChild("by"))
 local compileSubmodulesToArray = require(common.util:WaitForChild("compileSubmodulesToArray"))
 local assets = compileSubmodulesToArray(script, true)
 
+for _, asset in pairs(assets) do
+    asset.shopCatagory = asset.shopCatagory or asset.type
+end
+
 local function isLessRare(asset1,asset2)
     return asset1.rarity < asset2.rarity
 end
