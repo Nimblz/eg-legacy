20a5ea6f2501abb6eebf9b73e5891c1bb32a89e1
Austin Reuschle <areuschle@roblox.local>
2019-10-17 15:29:49 -0700

    Add currency handling

--- src/common/Thunks/ASSET_TRYBUY.lua
+++ src/common/Thunks/ASSET_TRYBUY.lua
@@ -8,13 +8,21 @@ local ShopProducts = require(common:WaitForChild("ShopProducts"))
 local Selectors = require(script.Parent.Parent:WaitForChild("Selectors"))
 local Actions = require(script.Parent.Parent:WaitForChild("Actions"))
 
+local currencyActions = {
+    coins = Actions.COIN_SUBTRACT,
+    candy = Actions.CANDY_SUBTRACT,
+}
+
 return function(player,assetId)
     return function(store)
         local product = ShopProducts.byId[assetId]
         if product then
+            local currencyType = product.currency or "coins"
             if not Selectors.isOwned(store:getState(),player,assetId) then
-                if Selectors.getCoins(store:getState(),player) >= product.price then
-                    store:dispatch(Actions.COIN_SUBTRACT(player,product.price))
+                local stats = Selectors.getStats(store:getState(),player)
+                local ownedCurrency = stats[currencyType] or 0
+                if ownedCurrency >= product.price then
+                    store:dispatch(currencyActions[currencyType](player,product.price))
                     store:dispatch(Actions.ASSET_GIVE(player,assetId))
                 end
             end
